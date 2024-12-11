00b1d15fd2144c64235c7c91b7df0f0c6220df1d
chdickey <chdickey79@gmail.com>
2024-12-10 21:05:56 -0500

    Advent of Code day 9

--- day_6_old.py
+++ day_6_old.py
@@ -74,6 +74,20 @@ def move_obstrued(map, guard_position, guard_direction):
             obstrued = True
     return obstrued
 
+def guard_alignment(guard_direction):
+    guard_alignment = ""
+    if guard_direction == "^" or guard_direction == "v":
+        guard_alignment = "|"
+    elif guard_direction == ">" or guard_direction == "<":
+        guard_alignment = "-"
+    return guard_alignment
+
+def path_alignment(path_alignment, guard_alignment):
+    new_alignment = guard_alignment
+    if (path_alignment == "|" and guard_alignment == "-") or (path_alignment == "-" and guard_alignment == "|"):
+        new_alignment = "+"
+    return new_alignment
+
 def other_branch_exist(map, guard_position, guard_direction):
     branch_found = False
     if guard_direction == "^":
@@ -124,6 +138,58 @@ def other_branch_exist(map, guard_position, guard_direction):
                 #print(obstruction_position)
                 #if input(guard_direction) == "q":
                 #    exit()
+    return branch_found
+
+def other_branch_exist2(map, guard_position, guard_direction):
+    branch_found = False
+    if guard_direction == "^":
+        obstruction_position = nu.where(map[guard_position[0], guard_position[1]:] == "#")
+        path_position = nu.where(map[guard_position[0], guard_position[1]:] == "-")
+        path_position2 = nu.where(map[guard_position[0], guard_position[1]:] == "+")
+        if len(obstruction_position[0]) > 0 and ((len(path_position[0]) > 0 and path_position[0][0] < obstruction_position[0][0]) or \
+            (len(path_position2[0]) > 0 and path_position2[0][0] < obstruction_position[0][0])):
+            branch_found = True
+    elif guard_direction == ">":
+        obstruction_position = nu.where(map[guard_position[0]:, guard_position[1]] == "#")
+        path_position = nu.where(map[guard_position[0]:, guard_position[1]] == "|")
+        path_position2 = nu.where(map[guard_position[0]:, guard_position[1]] == "+")
+        if len(obstruction_position[0]) > 0 and ((len(path_position[0]) > 0 and path_position[0][0] < obstruction_position[0][0]) or \
+            (len(path_position2[0]) > 0 and path_position2[0][0] < obstruction_position[0][0])):
+            branch_found = True
+    elif guard_direction == "v":
+        obstruction_position = nu.where(map[guard_position[0], :guard_position[1]] == "#")
+        path_position = nu.where(map[guard_position[0], :guard_position[1]] == "-")
+        path_position2 = nu.where(map[guard_position[0], :guard_position[1]] == "+")
+        if len(obstruction_position[0]) > 0 and ((len(path_position[0]) > 0 and path_position[0][-1] > obstruction_position[0][-1]) or \
+            (len(path_position2[0]) > 0 and path_position2[0][-1] > obstruction_position[0][-1])):
+            branch_found = True
+    elif guard_direction == "<":
+        obstruction_position = nu.where(map[:guard_position[0], guard_position[1]] == "#")
+        path_position = nu.where(map[:guard_position[0], guard_position[1]] == "|")
+        path_position2 = nu.where(map[:guard_position[0], guard_position[1]] == "+")
+        if len(obstruction_position[0]) > 0 and ((len(path_position[0]) > 0 and path_position[0][-1] > obstruction_position[0][-1]) or \
+            (len(path_position2[0]) > 0 and path_position2[0][-1] > obstruction_position[0][-1])):
+            branch_found = True
+    return branch_found
+
+def other_branch_exist3(map, guard_position, guard_direction):
+    branch_found = False
+    if guard_direction == "^":
+        obstruction_position = nu.where(map[guard_position[0], guard_position[1]:] == "#")
+        if len(obstruction_position[0]) > 0:
+            branch_found = True
+    elif guard_direction == ">":
+        obstruction_position = nu.where(map[guard_position[0]:, guard_position[1]] == "#")
+        if len(obstruction_position[0]) > 0:
+            branch_found = True
+    elif guard_direction == "v":
+        obstruction_position = nu.where(map[guard_position[0], :guard_position[1]] == "#")
+        if len(obstruction_position[0]) > 0:
+            branch_found = True
+    elif guard_direction == "<":
+        obstruction_position = nu.where(map[:guard_position[0], guard_position[1]] == "#")
+        if len(obstruction_position[0]) > 0:
+            branch_found = True
     return branch_found
 
 def part_one():
@@ -190,7 +256,6 @@ def part_two():
 
 def part_two_brute_force():
     positions = 0
-    log = open("./day_6/day_6_log.txt", "w")
     with open("./day_6/day_6_input.txt") as file:
         lines = [list(line.strip()) for line in file.readlines()]
         map = nu.array(lines)
@@ -198,97 +263,54 @@ def part_two_brute_force():
         guard_direction = "^"
         start_position = nu.where(map == guard_direction)
         start_position = (int(start_position[0][0]), int(start_position[1][0]))
-        max_pos_row = nu.size(map, 0)
-        max_pos_col = nu.size(map, 1)
-        for obstruction_row in range(max_pos_row):
-            for obstruction_col in range(max_pos_col):
-                print(f"Test obstruction {obstruction_row:04d},{obstruction_col:04d} -> found={positions:04d}\r", end="")
-                if map[obstruction_row, obstruction_col] == ".":
-                    guard_position = start_position
-                    guard_direction = "^"
-                    obstruction_position = (obstruction_row, obstruction_col)
-                    map_test = nu.copy(map)
-                    map_test[obstruction_position[0], obstruction_position[1]] = "#"
-                    places_visited = 10
-                    places_walked = 0
-                    while guard_position[0] >= 0 and guard_position[1] >= 0 and guard_position[0] < max_pos_row and \
-                        guard_position[1] < max_pos_col and places_walked < 50000:
-                        places_walked += 1
-                        if map_test[guard_position[0], guard_position[1]] == ".":
-                            places_visited += 1
-                        if (map_test[guard_position[0], guard_position[1]] == guard_direction and places_walked > 1) or places_walked >= (places_visited * 2):
-                            #print(f"Obstruction found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}")
-                            log.write(f"Obstruction found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}\n")
-                            positions += 1
-                            if map_obtructions[obstruction_position[0], obstruction_position[1]] != "O":
-                                map_test[obstruction_position[0], obstruction_position[1]] = "O"
-                                #nu.savetxt(".\\day_6\\trap_{%04d}x{%04d}.txt".format(obstruction_position[0], obstruction_position[1]), map_test, "%s", "")
-                            map_obtructions[obstruction_position[0], obstruction_position[1]] = "O"
-                            break
-                        map_test[guard_position[0], guard_position[1]] = guard_direction
-                        guard_position, guard_direction = move_guard2(map_test, guard_position, guard_direction)
-                    else:
-                        if places_walked >= 40000:
-                            print(f"No exit found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}")
-                            log.write(f"No exit found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}\n")
-    log.close()
-    print(f"Number of possibilities = {positions} obtructions")
-    positions = nu.count_nonzero(map_obtructions == "O")
-    print(f"Number of possibilities = {positions} obtructions")
-    return positions
-
-def part_two_brute_force_fast():
-    positions = 0
-    log = open("./day_6/day_6_log.txt", "w")
-    with open("./day_6/day_6_input.txt") as file:
-        lines = [list(line.strip()) for line in file.readlines()]
-        map = nu.array(lines)
-        map_obtructions = nu.copy(map)
-        guard_direction = "^"
-        start_position = nu.where(map == guard_direction)
-        start_position = (int(start_position[0][0]), int(start_position[1][0]))
-        max_pos_row = nu.size(map, 0)
-        max_pos_col = nu.size(map, 1)
-        map_path = nu.copy(map)
         guard_position = start_position
-        guard_position, guard_direction = move_guard2(map_path, guard_position, guard_direction)
+        max_pos_row = nu.size(map, 0)
+        max_pos_col = nu.size(map, 1)
+        #map[guard_position[0], guard_position[1]] = guard_direction
+        #guard_position, guard_direction = move_guard2(map, guard_position, guard_direction)
         while guard_position[0] >= 0 and guard_position[1] >= 0 and guard_position[0] < max_pos_row and guard_position[1] < max_pos_col:
-            map_path[guard_position[0], guard_position[1]] = "*"
-            guard_position, guard_direction = move_guard2(map_path, guard_position, guard_direction)
-        obstruction_positions = nu.where(map_path == "*")
-        for obstruction_no in range(len(obstruction_positions[0])):
-            obstruction_row = obstruction_positions[0][obstruction_no]
-            obstruction_col = obstruction_positions[1][obstruction_no]
-            print(f"Test obstruction {obstruction_row:04d},{obstruction_col:04d} -> found={positions:04d}\r", end="")
-            if map[obstruction_row, obstruction_col] == ".":
-                guard_position = start_position
-                guard_direction = "^"
-                obstruction_position = (obstruction_row, obstruction_col)
+            if not move_obstrued(map, guard_position, guard_direction): # and other_branch_exist2(map, guard_position, guard_direction):
+                obstruction_position, test_direction = move_guard2(map, guard_position, guard_direction)
+                #if guard_direction != test_direction:
+                #    continue
                 map_test = nu.copy(map)
                 map_test[obstruction_position[0], obstruction_position[1]] = "#"
-                places_visited = 10
-                places_walked = 0
-                while guard_position[0] >= 0 and guard_position[1] >= 0 and guard_position[0] < max_pos_row and \
-                    guard_position[1] < max_pos_col and places_walked < 50000:
-                    places_walked += 1
-                    if map_test[guard_position[0], guard_position[1]] == ".":
-                        places_visited += 1
-                    if (map_test[guard_position[0], guard_position[1]] == guard_direction and places_walked > 1) or places_walked >= (places_visited * 2):
-                        #print(f"Obstruction found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}")
-                        log.write(f"Obstruction found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}\n")
+                test_position = guard_position
+                test_direction = guard_direction
+                map_test[test_position[0], test_position[1]] = test_direction #path_alignment(map_test[test_position[0], test_position[1]], guard_alignment(test_direction))#test_direction
+                test_position, test_direction = move_guard2(map_test, test_position, test_direction)
+                iter_max = 20000
+                iter_nb = 0
+                while test_position[0] >= 0 and test_position[1] >= 0 and test_position[0] < max_pos_row and test_position[1] < max_pos_col and iter_nb < iter_max:
+                    iter_nb += 1
+                    if (test_position[0] == guard_position[0] and test_position[1] == guard_position[1] and (guard_direction == test_direction)) or \
+                        (map[test_position[0], test_position[1]] == test_direction):# or test_direction == turn_guard(guard_direction)):
                         positions += 1
                         if map_obtructions[obstruction_position[0], obstruction_position[1]] != "O":
                             map_test[obstruction_position[0], obstruction_position[1]] = "O"
-                            #nu.savetxt(".\\day_6\\trap_{%04d}x{%04d}.txt".format(obstruction_position[0], obstruction_position[1]), map_test, "%s", "")
+                            nu.savetxt(".\\day_6\\trap_{0:4}.txt".format(positions), map_test, "%s", "")
                         map_obtructions[obstruction_position[0], obstruction_position[1]] = "O"
+                        #if not (test_position[0] == (start_position[0]-1) and test_position[1] == start_position[1]):
+                        #if not (obstruction_position[0] == (start_position[0]) and obstruction_position[1] == start_position[1]):
+                        #    map_obtructions[obstruction_position[0], obstruction_position[1]] = "O"
+                        print(f"Number of possibilities = {positions} obtructions\r", end="")
+                        #print(map_test[test_position[0]-8:test_position[0]+8, test_position[1]-8:test_position[1]+8])
+                        #if input("direction=" + test_direction) == "q":
+                        #    exit()
                         break
-                    map_test[guard_position[0], guard_position[1]] = guard_direction
-                    guard_position, guard_direction = move_guard2(map_test, guard_position, guard_direction)
-                else:
-                    if places_walked >= 40000:
-                        print(f"No exit found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}")
-                        log.write(f"No exit found {obstruction_row:04d},{obstruction_col:04d} -> Walked={places_walked}, Visited={places_visited}\n")
-    log.close()
+                    map_test[test_position[0], test_position[1]] = test_direction #path_alignment(map_test[test_position[0], test_position[1]], guard_alignment(test_direction))#test_direction#test_direction
+                    test_position, test_direction = move_guard2(map_test, test_position, test_direction)
+                if iter_nb >= iter_max and 0 == 1:
+                    if map_obtructions[obstruction_position[0], obstruction_position[1]] != "O":
+                        map_test[obstruction_position[0], obstruction_position[1]] = "O"
+                        nu.savetxt(".\\day_6\\trap_z_{0:4}.txt".format(positions), map_test, "%s", "")
+                    positions += 1
+                    map_obtructions[obstruction_position[0], obstruction_position[1]] = "O"
+            map[guard_position[0], guard_position[1]] = guard_direction #path_alignment(map[guard_position[0], guard_position[1]], guard_alignment(guard_direction))#test_direction#guard_direction
+            if move_obstrued(map, guard_position, guard_direction):
+                guard_direction = turn_guard(guard_direction)
+            else:
+                guard_position, guard_direction = move_guard2(map, guard_position, guard_direction)
     print(f"Number of possibilities = {positions} obtructions")
     positions = nu.count_nonzero(map_obtructions == "O")
     print(f"Number of possibilities = {positions} obtructions")
@@ -303,5 +325,3 @@ if __name__ == "__main__":
         part_two()
     elif part == "3":
         part_two_brute_force()
-    elif part == "4":
-        part_two_brute_force_fast()
--- day_9_old.py
+++ day_9_old.py
@@ -57,42 +57,74 @@ def compress_data(compacted_data, start, end):
 def clean_data(file_data):
     start = 0
     while start < nu.size(file_data, 0):
-        if file_data[start][1] == 0: # file_data[start][0] == -1 
+        if file_data[start][0] == -1 and file_data[start][1] == 0:
             file_data = nu.delete(file_data, start, 0)
         else:
             start += 1
     return file_data
 
-def compress_file(file_data):
-    #start = 0
-    end = nu.size(file_data, 0) - 1
-    block_start = 0
-    while end > block_start:
+def compress_file(file_data, start):
+    counter = 0
+    new_start = start
+    end = nu.size(file_data, 0)
+    while start < end and new_start < end:
+        counter += 1
         block_size = -1
         block_id = -1
-        #print(f"End = {end}\r", end="")
-        if file_data[end][0] != -1:
-            block_start = 0
-            for i in range(0, end):
-                if file_data[i][0] == -1:
-                    if block_start == 0:
-                        block_start = i
-                    if file_data[i][1] >= file_data[end][1]:
-                        block_id = i
-                        block_size = file_data[i][1]
-                        break
-            if block_id >= 0 and block_size >= 0:
-                if file_data[block_id][1] == file_data[end][1]:
-                    file_data[block_id][0] = file_data[end][0]
-                    file_data[end][0] = -1
-                else:
-                    free_size = file_data[block_id][1] - file_data[end][1]
-                    file_data[block_id][0] = file_data[end][0]
-                    file_data[block_id][1] = file_data[end][1]
-                    file_data[end][0] = -1
-                    file_data = nu.insert(file_data, block_id+1, (-1, free_size), 0)
-                    end += 1
-        end -= 1
+        #print(f"Counter = {counter}\r", end="")
+        for i in range(start, end):
+            if file_data[i][0] == -1:
+                new_start = i
+                block_size = file_data[i][1]
+                break
+        else:
+            new_start = end
+        for i in range(end - 1, new_start , -1):
+            if file_data[i][0] != -1 and block_size >= file_data[i][1]:
+                block_id = i
+                break
+        #else:
+        #    new_start = end
+        #print((new_start, new_end))
+        if block_id >= 0 and block_size >= 0 and new_start < end:
+            free_size = 0
+            if block_size == file_data[block_id][1]:
+                file_data[new_start][0] = file_data[block_id][0]
+                file_data[block_id][0] = -1
+            else:
+                free_size = block_size - file_data[block_id][1]
+                file_data[new_start][0] = file_data[block_id][0]
+                file_data[new_start][1] = file_data[block_id][1]
+                file_data[block_id][0] = -1
+                file_data = nu.insert(file_data, new_start+1, (-1, free_size), 0)
+                new_start += 1
+            #    block_id += 1
+            #if file_data[block_id-1][0] == -1:
+            #    block_id -= 1
+            #    #file_data[block_id-1][1] += block_size
+            #    #file_data = nu.delete(file_data, block_id, 0)
+            while block_id < (end-1) and file_data[block_id][0] == -1 and file_data[block_id+1][0] == -1:
+                file_data[block_id][1] += file_data[block_id+1][1]
+                file_data = nu.delete(file_data, block_id+1, 0)
+                end = nu.size(file_data, 0)
+                if (block_id+1) < new_start:
+                    new_start -= 1
+            if free_size > 0:
+                while new_start < (end-1) and file_data[new_start][0] == -1 and file_data[new_start+1][0] == -1:
+                    file_data[new_start][1] += file_data[new_start+1][1]
+                    file_data = nu.delete(file_data, new_start+1, 0)
+                    end = nu.size(file_data, 0)
+            #if block_id >= 0 and block_id < start:
+            #    start = block_id + 1
+            #else:
+            #    file_data[block_id][0] = -1
+        #start = new_start
+        #print(block_id)
+        if block_id >= 0 and block_id < start:
+            start = block_id
+        else:
+            start += 1
+        end = nu.size(file_data, 0)
     return file_data
 
 def part_one():
@@ -121,8 +153,7 @@ def part_two():
         #print(file_data)
         file_data= clean_data(file_data)
         #print(file_data)
-        compacted_file = compress_file(file_data)
-        #print(compacted_file)
+        compacted_file = compress_file(file_data, 0)
         compacted_data = convert_file(compacted_file)
         #print(compacted_data)
         for i in range(len(compacted_data)):
